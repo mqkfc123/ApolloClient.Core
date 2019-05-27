37052fab7385bd83b1c8332c231277448137018b
liuyl <mqkfc123@163.com>
2019-05-27 13:54:54 +0800

    test

---
+++ Apollo.Core.WebApi/Controllers/ValuesController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Com.Ctrip.Framework.Apollo;
+using Com.Ctrip.Framework.Apollo.Model;
+
+namespace Apollo.Core.WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+        private IConfiguration _configuration;
+
+        public ValuesController(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+      
+        // GET api/values/5
+        [HttpGet]
+        public ActionResult<string> Get(string key)
+        {
+           
+            string title = _configuration.GetValue<string>("dev_test");
+            return title;
+        }
+
+     
+
+    }
+}
---
+++ Apollo.Core.WebApi/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+using Com.Ctrip.Framework.Apollo;
+
+namespace Apollo.Core.WebApi
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            CreateWebHostBuilder(args).Build().Run();
+        }
+
+        public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
+            WebHost.CreateDefaultBuilder(args)
+              .ConfigureAppConfiguration((hostingContext, builder) =>
+              {
+                  builder
+                  .AddApollo(builder.Build().GetSection("apollo"))
+                  .AddDefault() 
+                  .AddNamespace("application");
+              })
+              .UseStartup<Startup>();
+    }
+}
